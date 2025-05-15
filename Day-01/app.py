57107f9a278741be1125f5912c2d8f94b0994d46
Md Mazid Hossain <mazid.info67@gmail.com>
2025-05-15 13:29:55 +0600

    first day python practics

---
+++ Day-01/app.py
@@ -0,0 +1,86 @@
+# print("welcome to python world")
+
+# print (2+2)
+
+# print('''line1
+#           line2
+#            line3''')
+
+# name ="mazid" #variable
+# age=25  #variable
+
+# print (f'My name is {name} & age is {age}')  #string interpolation
+
+# print ("My nane is {} & my age is {}".format(name,age)) #string interpolation
+
+
+#Arithmatical operation 
+
+# print(5+2)
+# print(5-2)
+# print(5*2)
+# print(5/2)
+# print(5%2)
+# print(5**2)
+
+
+
+#str, int, bool(true,False)
+#Comparson opration
+
+
+# print(2==2) 
+# print(2!=2)
+# print(2>2)
+# print(2<2)
+# print(2>=3)
+# print(2<=3)
+
+
+#Logical Operator
+
+# x=2
+# y=7
+# z=15
+
+# print(y>x and y<x)
+# print(y>x or y<x)
+
+
+# #Member operator
+# print('Hello' in 'Hello there mazid')
+
+
+#user Input
+
+
+#Build in faction
+
+# name = input('type your name: ')
+# password=input("type your password:")
+
+# print(name)
+# # print(password)
+# num_1 = int (input("type your first number:"))
+# num_2 = int (input("type your second number:"))
+
+# print(num_1 + num_2)
+
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the Second number:"))
+
+# print("sum:", a + b)
+
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+
+# print("sum:", a + b)
+
+
+age = int(input("enter age: "))
+if age >= 18:
+    print("Adult")
+else:
+    print("Not Adult")
+    
