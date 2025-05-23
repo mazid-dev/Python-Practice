73bdeeeb9b381c977474a3bf0d494e837a54cb12
Md Mazid Hossain <mazid.info67@gmail.com>
2025-05-23 12:23:24 +0600

    change_file

---
+++ Day-04/app.py
@@ -0,0 +1,48 @@
+
+#Data type check            
+
+# x = 10
+# y = 3.5
+# z = "Hello"
+
+# print(type (x))
+# print(type (y))
+# print(type (z))
+
+
+
+#Logical Operators 
+
+# a = 10
+# b = 5
+
+# print(a > b)  # True
+# print(a == b)  # False
+# print(a > 0 and b > 0)  # True
+
+
+# #Input / Output
+
+# name = input ("apnar name ki? ")
+
+# print("hello",name)
+
+
+
+# Built-in Functions
+
+# text = "python"
+
+# print(len(text))
+# print(type(text))
+
+
+
+#test
+
+num1 = int(input("enter the first number"))
+num2 = int (input("enter the second number"))
+
+sum = num1 + num2
+
+print("equal", sum)
